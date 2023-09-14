4f7becb2e606dfa227077d5ab0b9c3efe1285b16
tauheedbuttt <tauheedbutt@gmail.com>
2023-09-14 12:02:51 +0500

    image upload middlewarE

--- helpers/image-upload.helper.js
+++ helpers/image-upload.helper.js
@@ -1,2 +1,13 @@
 const multer = require('multer');
-module.exports = multer({ storage: multer.diskStorage() })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+
+module.exports = multer({ storage: storage })
