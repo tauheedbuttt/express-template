037693c7b7ffc0f99cfedb275c6ad4bdd9cc1f9e
tauheedbuttt <tauheedbutt@gmail.com>
2023-09-15 16:09:56 +0500

    dynamic routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-// All Routes
-
-const routes = [
-    { to: 'auth', router: require('./auth.routes') },
-]
-
-
 const router = require("express").Router();
-
-routes.forEach(route => router.use(`/api/${route.to}`, route.router))
+const fs = require("fs")
 
 router.get("/", require("../controllers").welcome);
 
+const routes = fs.readdirSync(__dirname)
+    .filter((file) => file.indexOf(".") !== 0 && file.includes('routes'))
+    .map(item => item.split('.routes.js')[0])
+
+routes.forEach(route => router.use(
+    `/api/${route}`,
+    require(`./${route}.routes`)
+))
 module.exports = router
