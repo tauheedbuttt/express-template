3a4b927760e7ae787792e8dbf0195f512a73e2bd
tauheedbuttt <tauheedbutt@gmail.com>
2023-09-19 10:27:37 +0500

    dynamic sub folders routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,32 @@
 const router = require("express").Router();
 const fs = require("fs")
+const path = require("path")
 
 router.get("/", require("../controllers").welcome);
 
-const routes = fs.readdirSync(__dirname)
-    .filter((file) => file.indexOf(".") !== 0 && file.includes('routes'))
-    .map(item => item.split('.routes.js')[0])
+const routes = [];
+function getRoutes(directory, baseUrl = "") {
+    const files = fs.readdirSync(directory);
 
-routes.forEach(route => router.use(
-    `/api/${route}`,
-    require(`./${route}.routes`)
-))
+    files.forEach((file) => {
+        const filePath = path.join(directory, file);
+        const stats = fs.statSync(filePath);
+
+        if (stats.isDirectory()) {
+            const folderName = path.basename(filePath);
+            getRoutes(filePath, `${baseUrl}/${folderName}`);
+        } else if (file.endsWith(".routes.js")) {
+            routes.push(`${baseUrl}/${file.replace(".routes.js", "")}`)
+        }
+    });
+}
+
+getRoutes(__dirname)
+
+routes.forEach(route => {
+    router.use(
+        `/api${route}`,
+        require(`./${route}.routes`)
+    );
+})
 module.exports = router
