f911f5a59236f1752ad366bbe51b204ed970a540
tauheedbuttt <tauheedbutt@gmail.com>
2023-11-15 09:27:05 +0500

    socket middleware change

--- helpers/socket.helper.js
+++ helpers/socket.helper.js
@@ -1,9 +1,30 @@
 const cors = require('cors');
 const SocketJWT = require('../middlewares/authentication/socket.middleware');
+const Device = require('../models/Device');
 
 let io;
 
+const updateDevice = async (data) => {
+    const updated = await Device.findOneAndUpdate(
+        { user: data.user, device: data.device },
+        { ...data },
+        { new: true }
+    );
+    if (!updated) await Device.create(data)
+}
+
 const onConnect = (socket) => {
+    const { device, name, fcm, os } = socket.handshake?.query;
+
+    updateDevice({
+        user: socket.user._id,
+        device,
+        os,
+        fcm,
+        name,
+        socket: socket.id
+    })
+
     console.log("User Connected: " + socket.id);
     socket.on('disconnect', () => {
         console.log("User Disconnected");
@@ -29,8 +50,16 @@ const createServer = (app, port, host) => {
 
 const sendMessage = (socketID, event, message) => io.of('/api/socket').to(socketID).emit(event, message);
 
+const sendUserMessage = async (user, event, message) => {
+    const devices = await Device.find({ user });
+    devices.forEach(device => {
+        sendMessage(device.socket, event, message)
+    })
+}
+
 module.exports = {
     io,
     createServer,
-    sendMessage
+    sendMessage,
+    sendUserMessage
 }
